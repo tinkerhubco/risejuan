636b4d5e9532f8f9297daa15ac9d209ad2924c48
isotopeee <johnmichael.doroy@gmail.com>
2020-08-08 17:06:35 +0800

    feat: implement header functionalities (#26)
    * chore: add `@material-ui/icons` package

--- packages/client/src/pages/Login.js
+++ packages/client/src/pages/Login.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { Button } from '@material-ui/core';
+
+import { FixedHeader } from '../components/FixedHeader';
 
 export const Login = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return (
-    <Button variant="contained" onClick={loginWithRedirect}>
-      Proceed to login
-    </Button>
-  );
+  return <FixedHeader />;
 };
