7254300ded07ecfc90dd637ad39ff323caae645f
JM Santos <johnmichaelubas.santos@gmail.com>
2020-08-09 03:12:32 +0800

    ✨ feat: Add min post donor amount validation

--- packages/server/src/campaign/post-donor.dto.ts
+++ packages/server/src/campaign/post-donor.dto.ts
@@ -1,8 +1,17 @@
-import { IsNumber, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsNumber,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
+import { MINIMUM_AMOUNT_TO_DONATE } from '../constants/app';
 
 export class PostDonorDto {
   @IsNumber()
   @IsNotEmpty()
+  @Min(MINIMUM_AMOUNT_TO_DONATE)
   amount: number;
 
   @IsNotEmpty()
