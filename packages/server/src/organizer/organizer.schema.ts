9b3cc0a4b90d8855bb774bbade9759c67bf7568e
JM Santos <johnmichaelubas.santos@gmail.com>
2020-08-08 13:14:28 +0800

    ✨ feat: Added organizer schema

---
+++ packages/server/src/organizer/organizer.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Organizer extends Document {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  givenName: string;
+
+  @Prop()
+  familyName: string;
+
+  @Prop()
+  nickname: string;
+
+  @Prop()
+  picture: string;
+
+  @Prop()
+  locale: string;
+
+  @Prop()
+  sub: string;
+
+  @Prop()
+  emailVerified: boolean;
+
+  @Prop()
+  updatedAt: Date;
+}
+
+export const OrganizerSchema = SchemaFactory.createForClass(Organizer);
