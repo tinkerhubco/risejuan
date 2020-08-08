4301b7a0d664b8509c67ad51bf5e01ee125cfdc5
Ricky L. Hadjirul <hadjirulricky@gmail.com>
2020-08-08 15:44:23 +0800

    feat: Make attachment object (#20)
    * :recycle: feat: Make attachment object
    
    * Update packages/server/src/attachment/attachment.schema.ts
    
    * Update packages/server/src/attachment/attachment.schema.ts
    
    Co-authored-by: JM Santos <jmaicaaan@users.noreply.github.com>

---
+++ packages/server/src/attachment/attachment.schema.ts
@@ -0,0 +1,15 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Attachment extends Document {
+    @Prop()
+    url: string;
+
+    @Prop({
+      default: new Date()
+    })
+    createdDate: Date;
+}
+
+export const AttachmentSchema = SchemaFactory.createForClass(Attachment);
