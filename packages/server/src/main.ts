d26504d17e7e53aeeb7ac4d13b6b2094cd7d0161
JM Santos <johnmichaelubas.santos@gmail.com>
2020-08-09 21:23:25 +0800

    ✨ feat:  Https support

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,14 +4,32 @@ import { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 import ConfigUtil from './utils/config.util';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = getHttpsOptions();
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   const port = ConfigUtil.get('api.port');
 
   configureApp(app);
 
   await app.listen(port);
+}
+
+function getHttpsOptions() {
+  const privateKeyPath = process.env.HTTPS_PRIVATE_KEY_PATH;
+  const certPath = process.env.HTTPS_CERT_PATH;
+
+  if (!privateKeyPath || !certPath) {
+    return;
+  }
+
+  return {
+    key: readFileSync(privateKeyPath),
+    cert: readFileSync(certPath),
+  };
 }
 
 export function configureApp(app: INestApplication) {
